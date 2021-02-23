5b12d9cb75eb585d7f93f3b71e48640cceacf8c8
Devon Gennuso <devon.gennuso@gmail.com>
2021-02-23 16:35:31 -0600

    blue

---
+++ libs/ui/src/lib/presentation-components/index.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
+
+export const COMPONENTS: any[] = [
+    HeroBannerComponent
+];
